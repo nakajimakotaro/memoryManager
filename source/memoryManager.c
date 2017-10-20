15259b48abafba93942803a926596f9b0ed1f0bf
nakajimakotaro <harusann2@gmail.com>
2017-10-21 04:05:28 +0900

    型の修正

--- source/memoryManager.c
+++ source/memoryManager.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include "memoryManager.h"
 
 const size_t POOL_SIZE = 200;
 
 typedef struct _MemoryBlockHeader{
-    char* prevFreeBlock;
-    char* nextFreeBlock;
+    struct _MemoryBlockHeader* prevFreeBlock;
+    struct _MemoryBlockHeader* nextFreeBlock;
     size_t size;
-    int isUse;
+    bool isUse;
+    int8_t body[];
 } MemoryBlockHeader;
 typedef struct _MemoryPoolHeader{
-    char* startPoint;
-    char* lastPoint;
+    void* startPoint;
     MemoryBlockHeader* freeMemoryList;
 } MemoryPoolHeader;
 MemoryPoolHeader memoryPoolHeader = {};
 
 void initMemory(){
     memoryPoolHeader.startPoint = malloc(POOL_SIZE);
-    memoryPoolHeader.lastPoint = memoryPoolHeader.startPoint + POOL_SIZE;
     memoryPoolHeader.freeMemoryList = (MemoryBlockHeader*)memoryPoolHeader.startPoint;
     memoryPoolHeader.freeMemoryList->prevFreeBlock = NULL;
     memoryPoolHeader.freeMemoryList->nextFreeBlock = NULL;
     memoryPoolHeader.freeMemoryList->size = POOL_SIZE - sizeof(MemoryBlockHeader);
-    memoryPoolHeader.freeMemoryList->isUse = 0;
+    memoryPoolHeader.freeMemoryList->isUse = false;
 }
 
 void* myAllocate(size_t size){
     MemoryBlockHeader *newFreeMemoryHeader, *allocateMemoryHeader;
     MemoryBlockHeader tempMemoryHeader;
     allocateMemoryHeader = memoryPoolHeader.freeMemoryList;
-    do{
+    while(allocateMemoryHeader){
         if(allocateMemoryHeader->size >= sizeof(MemoryBlockHeader) + size){
             break;
         }
         allocateMemoryHeader = (MemoryBlockHeader*)allocateMemoryHeader->nextFreeBlock;
-    }while(allocateMemoryHeader);
+    }
     if(allocateMemoryHeader == NULL){
         return NULL;
     }
@@ -46,32 +47,42 @@ void* myAllocate(size_t size){
     allocateMemoryHeader->size = size;
     allocateMemoryHeader->prevFreeBlock = NULL;
     allocateMemoryHeader->nextFreeBlock = NULL;
-    allocateMemoryHeader->isUse = 1;
+    allocateMemoryHeader->isUse = true;
 
-    newFreeMemoryHeader = (MemoryBlockHeader*)((char*)allocateMemoryHeader + sizeof(MemoryBlockHeader) + allocateMemoryHeader->size);
+    newFreeMemoryHeader = (MemoryBlockHeader*)((int8_t*)allocateMemoryHeader + sizeof(MemoryBlockHeader) + allocateMemoryHeader->size);
     newFreeMemoryHeader->prevFreeBlock = tempMemoryHeader.prevFreeBlock;
     newFreeMemoryHeader->nextFreeBlock = tempMemoryHeader.nextFreeBlock;
     newFreeMemoryHeader->size = tempMemoryHeader.size - (allocateMemoryHeader->size + sizeof(MemoryBlockHeader));
-    newFreeMemoryHeader->isUse = 0;
+    newFreeMemoryHeader->isUse = false;
+
+    if(newFreeMemoryHeader->prevFreeBlock){
+        newFreeMemoryHeader->prevFreeBlock->nextFreeBlock = newFreeMemoryHeader;
+    }
+    if(newFreeMemoryHeader->nextFreeBlock){
+        newFreeMemoryHeader->nextFreeBlock->prevFreeBlock = newFreeMemoryHeader;
+    }
 
     memoryPoolHeader.freeMemoryList = newFreeMemoryHeader;
+    return allocateMemoryHeader->body;
 }
 
 void* myFree(){
+    return NULL;
 }
 
 void debugPoolView(){
-    printf("poolStart: %d\n", memoryPoolHeader.startPoint);
-    printf("poolLast:  %d\n", memoryPoolHeader.lastPoint);
-    printf("freePoint: %d\n", memoryPoolHeader.freeMemoryList);
-    printf("freeSize:  %d\n", memoryPoolHeader.freeMemoryList->size);
+    printf("poolStart: %ld\n", (size_t)memoryPoolHeader.startPoint);
+    printf("poolLast:  %ld\n", (size_t)((int8_t*)memoryPoolHeader.startPoint + POOL_SIZE));
+    printf("freePoint: %ld\n", (size_t)memoryPoolHeader.freeMemoryList);
+    printf("freeSize:  %ld\n", (size_t)memoryPoolHeader.freeMemoryList->size);
     printf("\n");
 }
-void main(){
+int main(){
     initMemory();
     debugPoolView();
     myAllocate(10);
     debugPoolView();
-    myAllocate(95);
+    myAllocate(30);
     debugPoolView();
+    return 0;
 }
